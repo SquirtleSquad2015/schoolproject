0c5a6ef0f3ab5388ebad7d12a04f79633009e1ed
martinp93 <martin.p@hotmail.no>
2015-04-14 16:38:40 +0200

    import IOException vet ikke hvorfor

--- Hoved.java
+++ Hoved.java
@@ -1,7 +1,8 @@
 package shoolprodject;
+import java.io.IOException;
 
 class Hoved {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         HovedVindu Vindumain = new HovedVindu();
         Vindumain.setLocationRelativeTo(null);
         Vindumain.setVisible(true);
