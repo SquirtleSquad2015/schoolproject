d442f6537dc54b9dd6751f41cc78712068c64b8b
martinp93 <martin.p@hotmail.no>
2015-04-17 11:13:53 +0200

    endret slik at den ikke gir feil

---
+++ shoolprodject/Hoved.java
@@ -0,0 +1,10 @@
+package shoolprodject;
+import java.io.IOException;
+
+class Hoved {
+    public static void main(String[] args) throws IOException {
+        HovedVindu Vindumain = new HovedVindu();
+        Vindumain.setLocationRelativeTo(null);
+        Vindumain.setVisible(true);
+    }
+}
