9fed8a7622297296547310cc6ad05a0c39e53477
jonasnr <jonasnr@student.hist.no>
2015-04-23 17:27:34 +0200

    CenterManager Update

--- CenterManagerMenu.java
+++ CenterManagerMenu.java
@@ -1,177 +1,172 @@
 package shoolprodject;
+
+import shoolprodject.DatabasePackage.Database;
+import shoolprodject.DatabasePackage.DatabaseConnection;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import shoolprodject.DatabasePackage.Database;
-import shoolprodject.DatabasePackage.DatabaseConnection;
-
+import java.util.ArrayList;
 import javax.swing.*;
-import javax.swing.border.Border;
 
 
 public class CenterManagerMenu extends JFrame {
-	
+
     private final String username;
     private String currentCenterName;
-
+    private ArrayList<String> stores = new ArrayList<String>();
     JButton viewShopCenterInfoBtn = new JButton("View center information");
-    JButton viewAdminInfoBtn = new JButton("View administrative information");
+    JButton viewAdminInfoBtn = new JButton("View annual turnover");
     JButton updateCenterInfoBtn = new JButton("Update center information");
-    JButton establishNewShopBtn = new JButton("Establish new shop");
-    JButton updateAccountInfoBtn = new JButton("Update account information");
     JButton updateStoreInfoBtn = new JButton("Update store information");
+    JButton updateAccountInfoBtn = new JButton("Update account information");
     JButton manageUsersBtn = new JButton("Manage Users");
     JButton exitBtn = new JButton("Exit");
 
-
-    // Administrative info Frame
-    JFrame viewAdminFrame = new JFrame();
-    JLabel turnoverLbl = new JLabel("Turnover", JLabel.CENTER);
-    JLabel showTurnoverBtn = new JLabel("A milli", JLabel.CENTER);
-    JButton backBtn = new JButton("Back");
-
-    //Update center info frame
-    JFrame updateCenterFrame = new JFrame();
-    JLabel centerName = new JLabel("", JLabel.CENTER);
-    JLabel sqm = new JLabel("", JLabel.CENTER);
-    JLabel nrOfShops = new JLabel("", JLabel.CENTER);
-    JLabel carPark = new JLabel("", JLabel.CENTER);
-    JLabel mail = new JLabel("Update center email", JLabel.CENTER);
-    JLabel phoneNumber = new JLabel("Update center phone number", JLabel.CENTER);
-    JTextArea description = new JTextArea();
-    JLabel descriptionTitle = new JLabel("Description", JLabel.CENTER);
-    JButton updateCenterNameBtn = new JButton("Update center name");
-    JButton updateCenterAreaBtn = new JButton("Update center area");
-    JButton updateCenterNrOfShopsBtn = new JButton("Update nr of shops");
-    JButton updateCarParkBtn = new JButton("Update car park");
-    JButton updateMail = new JButton("Update center email");
-    JButton updatePhoneNumber = new JButton("Update center phone number");
-    JButton updateDescriptionBtn = new JButton("Update description");
-    JButton updateCenterBackBtn = new JButton("Back");
-
-    // Establish new shop frame
-
-    // Update Account info frame
-
-    //Manage users frame
-    JFrame manageUsersFrame = new JFrame();
-    JButton validateAccountBtn = new JButton("Validate Accounts");
-    JButton changeAccesslevelBtn = new JButton("Change access levels");
+    JFrame selectStoreFrame = new JFrame();
+    JPanel topPanel = new JPanel();
+    JPanel bottomPanel = new JPanel();
+    DefaultListModel defaultListModel = new DefaultListModel();
+    JList storeList = new JList(defaultListModel);
+    JScrollPane scrollPane = new JScrollPane(storeList);
+    JButton selectStore = new JButton("Next");
+    JButton selectStoreBack = new JButton("Back");
 
     public CenterManagerMenu(String username){
-        super("Center Manager -" + username);
+        super("Center manager - " + username);
         this.username = username;
-        LayoutManager layout = new GridLayout(3,3,2,2);
+        LayoutManager layout = new GridLayout(7,1,2,2);
+        setVisible(true);
+        setLocationRelativeTo(null);
         setLayout(layout);
         add(viewShopCenterInfoBtn);
         add(viewAdminInfoBtn);
         add(updateCenterInfoBtn);
-        add(establishNewShopBtn);
-        add(updateAccountInfoBtn);
+        add(updateStoreInfoBtn);
         add(manageUsersBtn);
+        add(updateAccountInfoBtn);
         add(exitBtn);
         pack();
-
         Action action = new Action();
         viewShopCenterInfoBtn.addActionListener(action);
         viewAdminInfoBtn.addActionListener(action);
         updateCenterInfoBtn.addActionListener(action);
-        establishNewShopBtn.addActionListener(action);
+        updateStoreInfoBtn.addActionListener(action);
         updateAccountInfoBtn.addActionListener(action);
         manageUsersBtn.addActionListener(action);
         exitBtn.addActionListener(action);
-        // Update center information ---------------------------------------------
-        Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
-        description.setBorder(border);
-        description.setPreferredSize(new Dimension(300, 100));
-        description.setLineWrap(true);
-        description.setWrapStyleWord(true);
-        description.setEditable(false);
-        description.setOpaque(false);
-        JPanel updateCenterTop = new JPanel();
-        JPanel updateCenterCenter = new JPanel();
-        JPanel updateCenterBottom = new JPanel();
-        LayoutManager updateCenterLayout = new BorderLayout();
-        LayoutManager updateCenterTopLayout = new GridLayout(6,2,3,3);
-        LayoutManager updateCenterCenterLayout = new GridLayout(1,2,3,3);
-        LayoutManager updateCenterBottomLayout = new GridLayout(1,2,3,3);
-        updateCenterFrame.setLayout(updateCenterLayout);
-        updateCenterTop.setLayout(updateCenterTopLayout);
-        updateCenterCenter.setLayout(updateCenterCenterLayout);
-        updateCenterBottom.setLayout(updateCenterBottomLayout);
-        updateCenterTop.add(centerName);
-        updateCenterTop.add(updateCenterNameBtn);
-        updateCenterTop.add(nrOfShops);
-        updateCenterTop.add(updateCenterNrOfShopsBtn);
-        updateCenterTop.add(sqm);
-        updateCenterTop.add(updateCenterAreaBtn);
-        updateCenterTop.add(mail);
-        updateCenterTop.add(updateMail);
-        updateCenterTop.add(phoneNumber);
-        updateCenterTop.add(updatePhoneNumber);
-        updateCenterTop.add(carPark);
-        updateCenterTop.add(updateCarParkBtn);
-        updateCenterCenter.add(descriptionTitle);
-        updateCenterCenter.add(description);
-        updateCenterBottom.add(updateCenterBackBtn);
-        updateCenterBottom.add(updateDescriptionBtn);
-        updateCenterFrame.add(updateCenterTop, BorderLayout.NORTH);
-        updateCenterFrame.add(updateCenterCenter, BorderLayout.CENTER);
-        updateCenterFrame.add(updateCenterBottom, BorderLayout.SOUTH);
-        updateCenterFrame.pack();
+
+
+        LayoutManager borderLayout = new BorderLayout();
+        LayoutManager topLayout = new GridLayout(1,1,0,0);
+        LayoutManager bottomLayout = new GridLayout(1,2,0,0);
+        selectStoreFrame.setLayout(borderLayout);
+        topPanel.setLayout(topLayout);
+        bottomPanel.setLayout(bottomLayout);
+        topPanel.add(scrollPane);
+        bottomPanel.add(selectStoreBack);
+        bottomPanel.add(selectStore);
+        selectStoreFrame.add(topPanel, BorderLayout.NORTH);
+        selectStoreFrame.add(bottomPanel, BorderLayout.SOUTH);
+        selectStoreFrame.pack();
+        ActionUpdateShop actionUpdateShop = new ActionUpdateShop();
+        selectStore.addActionListener(actionUpdateShop);
+        selectStoreBack.addActionListener(actionUpdateShop);
     }
-	private class Action extends DatabaseConnection implements ActionListener {
+
+    private class Action extends DatabaseConnection implements ActionListener {
+
         @Override
         public void actionPerformed(ActionEvent actionEvent)  {
             if (actionEvent.getSource() == viewShopCenterInfoBtn) {
-                // Insert Customer
+                Customer customer = new Customer();
+                customer.setVisible(true);
+                customer.setLocationRelativeTo(null);
             } else if (actionEvent.getSource() == viewAdminInfoBtn) {
-
+                try {
+                    openConnection();
+                    currentCenterName = getCenter(username);
+                    closeConnection();
+                }
+                catch(Exception e){
+                    Database.printMesssage(e, "turnover");
+                }
+                ViewAnnualTurnover viewAnnualTurnover = new ViewAnnualTurnover(currentCenterName);
+                viewAnnualTurnover.setVisible(true);
+                viewAnnualTurnover.setLocationRelativeTo(null);
+                
             } else if (actionEvent.getSource() == updateCenterInfoBtn) {
                 try {
                     openConnection();
                     currentCenterName = getCenter(username);
-                    String currentNrofShops =getNoOfShops(currentCenterName);
-                    String currentSqm = getSQM(currentCenterName);
-                    String currentMail = getCenterMail(currentCenterName);
-                    String currentPhoneNumber = getCenterTelephone(currentCenterName);
-
-                    centerName.setText("Center name: " + currentCenterName);
-                    nrOfShops.setText("Number of shops: " + currentNrofShops);
-                    sqm.setText("Number of sqm: " + currentSqm);
-                    mail.setText("Email: " + currentMail);
-                    phoneNumber.setText("Phone Number: " + currentPhoneNumber);
+                    UpdateCenterInfo updateCenterInfo = new UpdateCenterInfo(username, currentCenterName);
+                    updateCenterInfo.setVisible(true);
+                    updateCenterInfo.setLocationRelativeTo(null);
                     closeConnection();
                 }
                 catch (Exception e){
                     Database.printMesssage(e, "UpdateCenterInfoBtn");
                 }
-                updateCenterFrame.setVisible(true);
-                updateCenterFrame.setLocationRelativeTo(null);
             }
-            else if (actionEvent.getSource() == establishNewShopBtn) {
-
-            } else if (actionEvent.getSource() == updateAccountInfoBtn) {
+            else if (actionEvent.getSource() == updateAccountInfoBtn) {
+                String tlf = "";
+                String email = "";
                 try {
                     openConnection();
-                    String email = getEmail(username);
-                    String tlf = getPhoneNumber(username);
-                    //currentEmail.setText(email);
-                    //currentPhoneNumber.setText(tlf);
+                    email = getEmail(username);
+                    tlf = getPhoneNumber(username);
                     closeConnection();
                 } catch (Exception e) {
                     Database.printMesssage(e, "changeAccountInfo");
                 }
-                setVisible(false);
-                //changeAccountInfoFrame.setVisible(true);
-                //changeAccountInfoFrame.setLocationRelativeTo(null);
+                ChangeAccountInfo changeAccountInfo = new ChangeAccountInfo(username, email, tlf);
+                changeAccountInfo.setVisible(true);
+                changeAccountInfo.setLocationRelativeTo(null);
             } else if (actionEvent.getSource() == manageUsersBtn) {
 
             }
             else if(actionEvent.getSource() == updateStoreInfoBtn){
+                try {
+                    openConnection();
+                    currentCenterName = getCenter(username);
+                    defaultListModel.clear();
+                    stores = getStore(currentCenterName);
+                    for(int i = 0; i < stores.size(); i++){
+                        defaultListModel.addElement(stores.get(i));
+                    }
+                    closeConnection();
+                }
+                catch (Exception e){
+                    Database.printMesssage(e, "updateStoreInfo");
+                }
+                selectStoreFrame.setTitle("Select store - " + currentCenterName);
+                selectStoreFrame.setVisible(true);
+                selectStoreFrame.setLocationRelativeTo(null);
 
             } else {
                 System.exit(0);
+            }
+        }
+    }
+    public class ActionUpdateShop extends DatabaseConnection implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            if(actionEvent.getSource() == selectStore){
+                int index = storeList.getSelectedIndex();
+                try {
+                    openConnection();
+                    System.out.println(currentCenterName);
+                    System.out.println(stores.get(index));
+                    UpdateStoreInfo updateStoreInfo = new UpdateStoreInfo(currentCenterName, stores.get(index));
+                    updateStoreInfo.setVisible(true);
+                    updateStoreInfo.setLocationRelativeTo(null);
+                    closeConnection();
+                }
+                catch (Exception e){
+
+                }
+            } else {
+                selectStoreFrame.dispose();
             }
         }
     }
