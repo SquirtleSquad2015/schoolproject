b97e8901adc8a4b9aba950915e1dba5169894403
haldorm <haldormyre@gmail.com>
2015-04-28 18:45:51 +0200

    deletecener

--- deleteCenter.java
+++ deleteCenter.java
@@ -71,7 +71,7 @@ public class deleteCenter extends JFrame {
             if (check ==delete){
                 try{
                     openConnection();//må opprette sin egen, max 1 extends per klasse
-                    int i=deleteCenter(centerName);
+                    int i = deleteCenter(centerName);
                     closeConnection();
                     
                     dispose();
